584613127ca290ce1bc6f3206874b3e3614adc7c
pravinc3798 <pravin.choudhary88@gmail.com>
2023-06-02 19:07:05 +0530

    added display method

--- CustomHashTable/CustomHashTable/CustomHashTable.cs
+++ CustomHashTable/CustomHashTable/CustomHashTable.cs
@@ -29,4 +29,20 @@ public class CustomHashTable
         newNode.Next = Names[arrayIndex];
         Names[arrayIndex] = newNode;
     }
+
+    public void Display()
+    {
+        for (int index = 0; index < Names.Length; index++)
+        {
+            Node temp = Names[index];
+
+            Console.Write($"Index {index} : ");
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " -> ");
+                temp = temp.Next;
+            }
+            Console.WriteLine();
+        }
+    }
 }
--- CustomHashTable/CustomHashTable/Program.cs
+++ CustomHashTable/CustomHashTable/Program.cs
@@ -2,5 +2,15 @@
 
 internal class Program
 {
+    static void Main()
+    {
+        CustomHashTable myHashTable = new CustomHashTable(10);
+        myHashTable.Add("Bob");
+        myHashTable.Add("Max");
+        myHashTable.Add("John");
+        myHashTable.Add("Peter");
 
+        myHashTable.Display();
+
+    }
 }
